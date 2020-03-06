cc3f2eed8e2d2e16655834f1131c3931a12e6af5
artemowkin <artemowa@tutanota.com>
2020-03-07 04:03:04 +0900

    1.1.3-2 add mixin into accounts forms

--- history/accounts/forms.py
+++ history/accounts/forms.py
@@ -1,25 +1,31 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
-class UserForm(UserCreationForm):
+class WidgetAttrsMixin:
+    def add_widget_attr(self, field, attr_name, value):
+        field.widget.attrs[attr_name] = value
+
+
+class UserForm(UserCreationForm, WidgetAttrsMixin):
     def get_initial_for_field(self, field, field_name):
         if field_name == 'username':
-            field.widget.attrs['placeholder'] = 'Your nickname'
+            self.add_widget_attr(field, 'placeholder', 'Your nickname')
         elif field_name == 'password1':
-            field.widget.attrs['placeholder'] = 'Your password'
+            self.add_widget_attr(field, 'placeholder', 'Your password')
         elif field_name == 'password2':
-            field.widget.attrs['placeholder'] = 'Confirm your password'
+            self.add_widget_attr(field, 'placeholder',
+                    'Confirm your password')
 
-        field.widget.attrs['class'] = 'form-input__input'
+        self.add_widget_attr(field, 'class', 'form-input__input')
         super().get_initial_for_field(field, field_name)
 
 
-class LoginForm(AuthenticationForm):
+class LoginForm(AuthenticationForm, WidgetAttrsMixin):
     def get_initial_for_field(self, field, field_name):
         if field_name == 'username':
-            field.widget.attrs['placeholder'] = 'Your nickname'
+            self.add_widget_attr(field, 'placeholder', 'Your nickname')
         elif field_name == 'password':
-            field.widget.attrs['placeholder'] = 'Your password'
+            self.add_widget_attr(field, 'placeholder', 'Your password')
 
         field.widget.attrs['class'] = 'form-input__input'
         super().get_initial_for_field(field, field_name)
