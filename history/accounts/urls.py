0e9e3a8e7aa828eaede4b6233989a2b4fa7e9955
artemowkin <artemowa@tutanota.com>
2020-03-07 23:25:27 +0900

    fix urls in templates

--- history/accounts/urls.py
+++ history/accounts/urls.py
@@ -3,9 +3,12 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name = 'accounts'
 urlpatterns = [
     path('registration/', views.UserCreateView.as_view(),
         name='registration'),
     path('login/', views.MyLoginView.as_view(), name='login'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
 ]
